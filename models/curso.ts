c97c63057e420081b3026e284a06837b5eb2e90a
caiocatarino <caiocatarino7@hotmail.com>
2020-03-25 09:27:27 -0300

    Arquivos com o .ts

---
+++ models/curso.ts
@@ -0,0 +1,87 @@
+﻿import Sql = require("../infra/sql");
+
+export = class Curso {
+	public idCurso: number;
+	public nomeCurso: string;
+	public responsavelCurso: string;
+	public horasSemanaisCurso: string;
+	public descricaoCurso: string;
+
+	// private static validar(c: Contato): string {
+	// 	c.nome = (c.nome || "").trim().toUpperCase();
+	// 	if (c.nome.length < 3 || c.nome.length > 200)
+	// 		return "Nome inválido";
+	// 	c.endereco = (c.endereco || "").trim().toUpperCase();
+	// 	if (c.endereco.length < 3 || c.endereco.length > 200)
+	// 		return "Endereço inválido";	
+	// 	c.email = (c.email || "").trim().toUpperCase();
+	// 	if (c.email.length < 3 || c.email.length > 200)
+	// 			return "Email inválido";
+	// 	if (c.peso <= 0)
+	// 		return "Peso inválido!";
+	// 	return null;
+	// }
+
+	public static async listar(): Promise<Curso[]> {
+		let lista: Curso[] = null;
+
+		await Sql.conectar(async (sql: Sql) => {
+			lista = await sql.query("select idCurso, nomeCurso, responsavelCurso, horasSemanaisCurso, duracaoCurso, "
+			+ " descricaoCurso from curso order by nomeCurso asc") as Curso[];
+		});
+
+		return (lista || []);
+	}
+
+	// public static async obter(id: number): Promise<Contato> {
+	// 	let lista: Contato[] = null;
+
+	// 	await Sql.conectar(async (sql: Sql) => {
+	// 		lista = await sql.query("select id, nome, endereco, email, peso from contato where id = ?",[id]) as Contato[];
+	// 	});
+
+	// 	if (lista && lista[0]) {
+	// 		return lista[0];
+	// 	}else {
+	// 		return null;
+	// 	}
+
+	// 	//return ((lista && lista[0]) || null);
+	// }
+
+	// public static async criar(c: Contato): Promise<string> {
+	// 	let res: string;
+	// 	if ((res = Contato.validar(c)))
+	// 		return res;
+
+	// 	await Sql.conectar(async (sql: Sql) => {
+	// 			await sql.query("insert into contato (nome,endereco,email,peso) values (?,?,?,?)", [c.nome,c.endereco,c.email,c.peso]);
+	// 	});
+
+		
+	// }
+
+	// public static async alterar(c: Contato): Promise<string> {
+	// 	let res: string;
+	// 	if ((res = Contato.validar(c)))
+	// 		return res;
+
+	// 	await Sql.conectar(async (sql: Sql) => {	
+	// 			await sql.query("update contato set nome = ?, endereco = ?, email = ?, peso = ? where id = ?", [c.nome,c.endereco,c.email,c.peso,c.id]);
+	// 			res = sql.linhasAfetadas.toString();
+	// 	});
+
+		
+	// }
+
+	// public static async excluir(id: number): Promise<string> {
+	// 	let res: string = null;
+
+	// 	await Sql.conectar(async (sql: Sql) => {
+	// 		await sql.query("delete from contato where id = " + id);
+	// 		res = sql.linhasAfetadas.toString();
+	// 	});
+
+	// 	return res;
+	// }
+}
--- routes/curso.js
+++ routes/curso.js
@@ -1,1 +1,41 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const express = require("express");
+const wrap = require("express-async-error-wrapper");
+//import Usuario = require("../models/usuario");
+const Curso = require("../models/curso");
+const router = express.Router();
+// router.all("/criar", wrap(async (req: express.Request, res: express.Response) => {
+// 	let u = await Usuario.cookie(req);
+// 	if (!u || !u.admin) {
+// 		res.redirect("/acesso");
+// 	} else {
+// 		res.render("contato/alterar", { titulo: "Criar Contato", usuario: u, item: null });
+// 	}
+// }));
+// router.all("/alterar", wrap(async (req: express.Request, res: express.Response) => {
+// 	let u = await Usuario.cookie(req);
+// 	if (!u || !u.admin) {
+// 		res.redirect("/acesso");
+// 	} else {
+// 		let id = parseInt(req.query["id"]);
+// 		let item: Contato = null;
+// 		if (isNaN(id) || !(item = await Contato.obter(id)))
+// 			res.render("shared/nao-encontrado", { usuario: u });
+// 		else
+// 			res.render("contato/alterar", { titulo: "Editar Contato", usuario: u, item: item });
+// 	}
+// }));
+router.get("/listar", wrap((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.render("contato/listar", { lista: JSON.stringify(yield Curso.listar()) });
+})));
+module.exports = router;
 //# sourceMappingURL=curso.js.map
